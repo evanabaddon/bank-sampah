66b10f19e2e4ec031a56b70d15d4cc49d16cae03
evanabaddon <evanabaddon@gmail.com>
2023-10-05 01:16:48 +0700

    perbaikian tombol cetak kartu

--- resources/views/nasabah/detail.blade.php
+++ resources/views/nasabah/detail.blade.php
@@ -29,7 +29,7 @@
                   </li>
                 </ul>
                     {{-- tombol cetak kartu anggota --}}
-                    <a href="{{ route('nasabah.cetakKartu', $model->id) }}" class="btn btn-primary btn-block" target="_blank"><b>Cetak Kartu Anggota</b></a>
+                    <a href="{{ route('nasabah.cetakKartu', $model->id) }}" class="btn btn-success btn-block" target="_blank"><b>Cetak Kartu Anggota</b></a>
                     {{-- <button class="btn btn-warning btn-block" data-toggle="modal" data-target="#confirmGeneratePinModal"><b>Buat PIN Baru</b></button>
                     <a href="{{ route('nasabah.kirim-pin', $model->id) }}" class="btn btn-success btn-block" target="_blank"><b>Kirim PIN</b></a> --}}
                     {{-- tombol transaksi bank sampah dengan mengirim id_nasabah --}}
